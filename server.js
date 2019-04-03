0fbc3d93abba319bb8e7b4193b5599ae4f92fae8
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-04-03 03:01:48 -0300

    Estrutura Inicial

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
-// const uri = "";
-
-// MongoClient.connect(uri, (err, client) => {
-//   // ... start the server
-// });
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+const uri = "mongodb://heroku_7tw4xsdw:i5vlsube7824c06kml6l5pl2fb@ds349455.mlab.com:49455/heroku_7tw4xsdw";
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const bd = mongoose.connection;
+bd.on('error', console.error.bind(console, 'Erro nessa porra:'));
+
+app.listen(3000, function() {
+    console.log('Este é seu primeiro servidor? Se for, parabéns, deu certo.');
+});
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.listen(3000, function() {
-    console.log('Este é seu primeiro servidor?');
-});
+app.post('/show', (req, res) => {
+    
+})
